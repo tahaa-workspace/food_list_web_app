10c9c51e4ce7f02d60678e4bc52622cebbc53716
Tahaa Mandsorwala <tahaa.workspace@gmail.com>
2025-07-29 15:22:39 +0530

    form for adding item

--- food/urls.py
+++ food/urls.py
@@ -2,10 +2,13 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+app_name = 'food'
 urlpatterns = [
     # /food/
     path('', views.index, name='index'),
     # /food/1
     path('<int:item_id>/', views.detail, name='detail'),
     path('item/', views.item, name='item'),
+    # add item
+    path('add', views.create_item, name='create_item'),
 ]
