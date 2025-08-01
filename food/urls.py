44fdd914901fcd603fa12f9b1eb653a0a284353a
Tahaa Mandsorwala <tahaa.workspace@gmail.com>
2025-08-01 15:48:31 +0530

    implementing django signals and class based views

--- food/urls.py
+++ food/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     # /food/
     path('', views.index, name='index'),
     # /food/1
-    path('<int:item_id>/', views.detail, name='detail'),
+    path('<int:pk>/', views.FoodDetail.as_view(), name='detail'),
     path('item/', views.item, name='item'),
     # add item
     path('add', views.create_item, name='create_item'),
--- food/views.py
+++ food/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from .models import Item
 from django.template import loader
 from .forms import ItemForm
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 # Create your views here.
 def index(request):
@@ -22,6 +24,10 @@ def detail(request, item_id):
         'item': item,
     }
     return render(request, 'food/detail.html', context)
+
+class FoodDetail(DetailView):
+    model = Item
+    template_name = 'food/detail.html'
 
 def create_item(request):
     form = ItemForm(request.POST or None)
